dfc5d141fdee3635d4005de2955888d3a3880f8b
Allan Omondi <aomondi@strathmore.edu>
2025-09-25 16:41:11 +0300

    Create app.py

---
+++ huggingface-spaces-using-gradio/app.py
@@ -0,0 +1,28 @@
+import gradio as gr
+import joblib
+import numpy as np
+
+# Load trained model
+model = joblib.load("./model/decisiontree_classifier_baseline.pkl")
+
+# Define prediction function
+def predict(monthly_fee, customer_age, support_calls):
+    X = np.array([[monthly_fee, customer_age, support_calls]])
+    prediction = model.predict(X)
+    return int(prediction[0])
+
+# Build Gradio interface
+demo = gr.Interface(
+    fn=predict,
+    inputs=[
+        gr.Number(label="Monthly Fee"),
+        gr.Number(label="Customer Age"),
+        gr.Number(label="Support Calls")
+    ],
+    outputs=gr.Number(label="Predicted Class"),
+    title="Customer Churn Prediction",
+    description="Predict whether a customer will churn using a Decision Tree Classifier."
+)
+
+if __name__ == "__main__":
+    demo.launch()
